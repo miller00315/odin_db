f1b8e600b1ffbd7283e472f10f5f4b6c24da0cd4
Miller <miller00314@gmail.com>
2024-04-27 23:30:15 -0300

    adiconando o eestilo css a tabela

--- db/structure.sql
+++ db/structure.sql
@@ -50,6 +50,21 @@ CREATE TABLE user_site_route (
                 ON DELETE CASCADE
 );
 
+CREATE TABLE user_style_sheet (
+        id INT GENERATED ALWAYS AS IDENTITY,
+        uuid VARCHAR(100) UNIQUE NOT NULL,
+        style_sheet_url TEXT NOT NULL,
+        style_sheet_name VARCHAR(100) NOT NULL,
+        style_sheet_description TEXT,
+        user_uuid VARCHAR(100) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+
+        CONSTRAINT fk_user_table
+                FOREIGN KEY(user_uuid)
+                REFERENCES user_(uuid)
+                ON DELETE CASCADE   
+);
+
 CREATE TABLE api_access(
         id INT GENERATED ALWAYS AS IDENTITY,
         uuid VARCHAR(100) UNIQUE NOT NULL,
